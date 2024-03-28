4a2a237ff5bb6f6119f8fbed7c58424448d64c22
Ben Champagne <donkeykong797@gmail.com>
2024-03-28 18:02:57 -0400

    restarting

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,46 +3,32 @@
 
 use std::sync::Mutex;
 
-use tauri::State;
+use tauri::{Manager, State};
 use windows::Win32::{
     Foundation::HWND,
     UI::WindowsAndMessaging::{GetForegroundWindow, GetWindowTextA},
 };
 
-struct App {
-    win_last: Mutex<HWND>,
-    win_last_name: Mutex<String>,
+struct AppState(Mutex<App>);
+struct App {}
+
+impl App {
+    pub fn new() -> Self {
+        Self {}
+    }
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn macro_undo(app: State<App>) -> Result<(String, String), ()> {
-    let mut win_last = app.win_last.lock().unwrap();
-    let mut win_last_name = app.win_last_name.lock().unwrap();
-    let last_name = win_last_name.clone();
-
-    unsafe {
-        let win: HWND;
-        let lpstring = &mut vec![0; 255].into_boxed_slice();
-        let name: String;
-
-        win = GetForegroundWindow();
-        name = GetWindowTextA(win, lpstring).to_string();
-
-        *win_last = win;
-        *win_last_name = name.clone();
-
-        return Ok((name, last_name));
-    };
+fn macro_undo(app_state: State<App>) -> Result<(), ()> {
+    Ok(())
 }
 
 fn main() {
-    let app = App {
-        win_last: Mutex::new(HWND(0)),
-        win_last_name: Mutex::new("".to_owned()),
-    };
+    let app_state: AppState = AppState(Mutex::new(App::new()));
+
     tauri::Builder::default()
-        .manage(app)
+        .manage(app_state)
         .invoke_handler(tauri::generate_handler![macro_undo])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
