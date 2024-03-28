18e9c77399b15a99e14509e78850afe45d019cb0
Ben Champagne <donkeykong797@gmail.com>
2024-03-28 15:46:49 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { invoke } from "@tauri-apps/api";
 import "./App.scss";
 
 function App() {
+
+  const macroUndo = () => {
+    invoke("macro_undo").then(() => {
+      console.log("macro_undo: successfully invoked. Did it actually undo?")
+    }).catch(() => {
+      console.log("macro_undo: failed to invoke!")
+    })
+  }
+
   return (
     <>
+      <div><button onClick={macroUndo} style={{ width: "200px", height: "200px" }}>Undo</button></div>
     </>
   );
 }
