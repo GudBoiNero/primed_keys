45ea9be851c213282a0f3b4fedf62a633314fa81
Ben Champagne <donkeykong797@gmail.com>
2024-04-11 10:38:33 -0400

    more

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,21 +3,42 @@
 
 use std::sync::Mutex;
 
-use tauri::{Manager, State};
+use tauri::{
+    plugin::{Builder as PluginBuilder, TauriPlugin},
+    Manager, Runtime, State,
+};
 use windows::Win32::{
     Foundation::HWND,
     UI::WindowsAndMessaging::{GetForegroundWindow, GetWindowTextA},
 };
 
-pub struct App {}
+pub struct App {
+    hwnd: HWND,
+    hwnd_prev: HWND,
+}
 
 impl App {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            hwnd: HWND(0),
+            hwnd_prev: HWND(0),
+        }
     }
 }
 
 pub struct AppState(pub Mutex<App>);
+
+pub struct AppManager;
+impl AppManager {
+    pub fn init<R: Runtime>() -> TauriPlugin<R> {
+        PluginBuilder::new("process")
+            .on_event(|app, event| match event {
+                tauri::RunEvent::MainEventsCleared => {}
+                _ => {}
+            })
+            .build()
+    }
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -30,6 +51,7 @@ fn main() {
 
     tauri::Builder::default()
         .manage(app_state)
+        .plugin(AppManager::init())
         .invoke_handler(tauri::generate_handler![macro_undo])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
