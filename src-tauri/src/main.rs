385252738b50eba82e38dcedfc5c4e15ad307dfe
Ben Champagne <donkeykong797@gmail.com>
2024-04-11 11:08:42 -0400

    init

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::sync::Mutex;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 use tauri::{
     plugin::{Builder as PluginBuilder, TauriPlugin},
-    Manager, Runtime, State,
+    Runtime, State,
 };
 use windows::Win32::{
     Foundation::HWND,
@@ -26,33 +30,52 @@ impl App {
     }
 }
 
-pub struct AppState(pub Mutex<App>);
-
-pub struct AppManager;
-impl AppManager {
-    pub fn init<R: Runtime>() -> TauriPlugin<R> {
-        PluginBuilder::new("process")
-            .on_event(|app, event| match event {
-                tauri::RunEvent::MainEventsCleared => {}
-                _ => {}
-            })
-            .build()
+pub struct AppState(pub Arc<Mutex<App>>);
+impl AppState {
+    pub fn init() -> Self {
+        AppState(Arc::new(Mutex::new(App::new())))
     }
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn macro_undo(app_state: State<App>) -> Result<(), ()> {
+fn macro_undo(state: State<AppState>) -> Result<(), ()> {
+    Ok(())
+}
+
+#[tauri::command]
+// https://github.com/tauri-apps/tauri/discussions/4775
+fn init<R: Runtime>(window: tauri::Window<R>, state: State<'_, AppState>) -> Result<(), String> {
+    let state = Arc::clone(&state.0);
+
+    thread::spawn(move || loop {
+        let lock = state.try_lock();
+        // Handle errors, unwrap if you want
+        if lock.is_err() {
+            break;
+        }
+        let lock = match lock {
+            Ok(x) => x,
+            Err(_) => break,
+        };
+
+        // Here's your state
+        let state = Box::new(lock).as_mut();
+
+        // use it however you want, you can emit an event to FE as well.
+        thread::sleep(Duration::from_millis(1000));
+        print!("Loop\n")
+    });
+
     Ok(())
 }
 
 fn main() {
-    let app_state: AppState = AppState(Mutex::new(App::new()));
+    let app_state: AppState = AppState::init();
 
     tauri::Builder::default()
         .manage(app_state)
-        .plugin(AppManager::init())
-        .invoke_handler(tauri::generate_handler![macro_undo])
+        .invoke_handler(tauri::generate_handler![init, macro_undo])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
