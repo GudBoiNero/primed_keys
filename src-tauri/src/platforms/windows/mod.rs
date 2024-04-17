810fc9e8db23cce69590d2e01f9266a7caa8de14
Ben Champagne <donkeykong797@gmail.com>
2024-04-17 19:39:29 -0400

    hwnd loop

--- src-tauri/src/platforms/windows/mod.rs
+++ src-tauri/src/platforms/windows/mod.rs
@@ -1,14 +1,15 @@
 use std::sync::MutexGuard;
 
-use windows::Win32::Foundation::HWND;
+use windows::Win32::{Foundation::HWND, UI::WindowsAndMessaging::GetForegroundWindow};
 
 use crate::platforms::app::App;
 
 #[derive(Default)]
 pub struct WinHandles {
-    pub current: HWND,
-    pub previous: HWND,
+    pub curr: HWND,
+    pub prev: HWND,
     pub target: HWND,
+    pub app: HWND,
 }
 
 pub struct WinApp {
@@ -28,4 +29,18 @@ impl App for WinApp {
     }
 }
 
-pub fn impl_win_update(state: &mut MutexGuard<WinApp>) {}
+pub fn impl_win_update(state: &mut MutexGuard<WinApp>) {
+    update_hwnd(state)
+}
+
+fn update_hwnd(state: &mut MutexGuard<WinApp>) {
+    state.handles.prev = state.handles.curr;
+    state.handles.curr = unsafe { GetForegroundWindow() };
+
+    if state.handles.curr != state.handles.app
+        && state.handles.curr != state.handles.prev
+        && state.handles.curr.0 != 0
+    {
+        state.handles.target = state.handles.curr;
+    }
+}
