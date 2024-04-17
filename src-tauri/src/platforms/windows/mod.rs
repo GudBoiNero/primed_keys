e2ac8192be8664a74173770c11ad45caf9b903ed
Ben Champagne <donkeykong797@gmail.com>
2024-04-17 19:35:46 -0400

    WinHandles

--- src-tauri/src/platforms/windows/mod.rs
+++ src-tauri/src/platforms/windows/mod.rs
@@ -1,13 +1,26 @@
 use std::sync::MutexGuard;
 
+use windows::Win32::Foundation::HWND;
+
 use crate::platforms::app::App;
+
+#[derive(Default)]
+pub struct WinHandles {
+    pub current: HWND,
+    pub previous: HWND,
+    pub target: HWND,
+}
 
 pub struct WinApp {
     pub initialized: bool,
+    pub handles: WinHandles,
 }
 impl App for WinApp {
     fn new() -> Self {
-        Self { initialized: false }
+        Self {
+            initialized: false,
+            handles: WinHandles::default(),
+        }
     }
 
     fn initialized(&self) -> bool {
