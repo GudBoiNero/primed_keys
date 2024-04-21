515f5f6add4ad92a658a61a7c36584832c6e20ca
Ben Champagne <donkeykong797@gmail.com>
2024-04-21 14:28:20 -0400

    refactor

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 #[cfg(target_os = "linux")]
 pub(crate) mod linux;
 #[cfg(target_os = "macos")]
