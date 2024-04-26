bba442f6c16ee96352e51d974fb0e36c585958cc
Ben Champagne <donkeykong797@gmail.com>
2024-04-26 11:28:58 -0400

    ThreadPriority

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -1,5 +1,10 @@
-// OS Independent Interface
+/// OS Independent Interface
 pub trait App {
     fn new() -> Self;
     fn initialized(&self) -> bool;
+}
+
+pub enum ThreadPriority {
+    Main = 0,
+    Command = 1,
 }
