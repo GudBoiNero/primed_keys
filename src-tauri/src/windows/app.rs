05b78c3bdabfdab8e59ecdece0c3ba313cdf4996
Ben Champagne <donkeykong797@gmail.com>
2024-05-08 09:21:42 -0400

    First AttachThreadInput never runs

--- src-tauri/src/windows/app.rs
+++ src-tauri/src/windows/app.rs
@@ -67,12 +67,11 @@ unsafe fn force_foreground_window(hwnd: HWND) {
         GetWindowThreadProcessId(GetForegroundWindow(), Some(std::ptr::null_mut()));
     let current_thread_id = GetCurrentThreadId();
     const CONST_SW_SHOW: i32 = 5;
-    AttachThreadInput(window_thread_process_id, current_thread_id, true);
-    println!("{:?}", GetLastError());
-    let _ = BringWindowToTop(hwnd);
-    ShowWindow(hwnd, SHOW_WINDOW_CMD(CONST_SW_SHOW));
-    AttachThreadInput(window_thread_process_id, current_thread_id, false);
-    println!("{:?}", GetLastError());
+    if AttachThreadInput(window_thread_process_id, current_thread_id, true).as_bool() {
+        let _ = BringWindowToTop(hwnd);
+        ShowWindow(hwnd, SHOW_WINDOW_CMD(CONST_SW_SHOW));
+        AttachThreadInput(window_thread_process_id, current_thread_id, false);
+    }
 }
 
 pub fn run_macro(state: &mut MutexGuard<OSApp>) {
