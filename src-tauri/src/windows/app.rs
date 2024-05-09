3d3214117b8cef30f9d15391330e0db9c2b19663
Ben Champagne <donkeykong797@gmail.com>
2024-05-09 08:30:46 -0400

    Remove prints

--- src-tauri/src/windows/app.rs
+++ src-tauri/src/windows/app.rs
@@ -69,37 +69,26 @@ fn update_hwnd(state: &mut MutexGuard<OSApp>) {
     state.handles.prev = state.handles.curr;
     state.handles.curr = unsafe { GetForegroundWindow() };
 
-    if (state.handles.prev != state.handles.curr) {
-        println!(
-            "curr: {:?} prev: {:?}",
-            state.handles.curr, state.handles.prev
-        );
-    }
-
     if state.handles.curr != state.handles.app
         && state.handles.curr != state.handles.prev
         && state.handles.curr.0 != 0
     {
         let prev = state.handles.target.clone();
         state.handles.target = state.handles.curr;
-        //println!("Changed Target: {} to {}", state.handles.target.0, prev.0);
+        println!("Changed Target: {:?} to {:?}", state.handles.target, prev);
     }
 }
 
-/// `AttachThreadInput` seems to have some issues in Rust. It always returns `WIN32_ERROR(87)` in this case. \
 /// Source: https://stackoverflow.com/a/59659421/17763366
 unsafe fn force_foreground_window(hwnd: HWND) {
     let current_thread_id = GetCurrentThreadId();
-    println!("current_thread_id: {:?}", current_thread_id);
     let window_thread_process_id = GetWindowThreadProcessId(hwnd, Some(null_mut()));
-    println!("window_thread_process_id: {:?}", window_thread_process_id);
     const CONST_SW_SHOW: i32 = 5;
     if AttachThreadInput(current_thread_id, window_thread_process_id, BOOL(1)).as_bool() {
         let _ = BringWindowToTop(hwnd);
         ShowWindow(hwnd, SHOW_WINDOW_CMD(CONST_SW_SHOW));
         AttachThreadInput(current_thread_id, window_thread_process_id, BOOL(0));
     }
-    println!("Error: {:?}", GetLastError());
 }
 
 pub fn run_macro(state: &mut MutexGuard<OSApp>) {
