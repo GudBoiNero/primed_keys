0ed5990c3fc032f086b44226756c7f2c9dc5a590
Ben Champagne <donkeykong797@gmail.com>
2024-05-12 10:51:43 -0400

    Attempting to Setup Message Loop

--- src-tauri/src/windows/app.rs
+++ src-tauri/src/windows/app.rs
@@ -5,25 +5,15 @@ use std::{
 
 use priomutex::MutexGuard;
 
-use windows::{
-    core::PCSTR,
-    Win32::{
-        Foundation::{GetLastError, BOOL, HMODULE, HWND, LPARAM, LRESULT, POINT, WPARAM},
-        System::{
-            LibraryLoader::{GetModuleHandleExA, GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS},
-            Threading::{AttachThreadInput, GetCurrentThreadId},
-        },
-        UI::{
-            Input::KeyboardAndMouse::{
-                SendInput, INPUT, INPUT_0, INPUT_KEYBOARD, KEYBDINPUT, KEYBD_EVENT_FLAGS,
-                KEYEVENTF_KEYUP, VK_LWIN,
-            },
-            WindowsAndMessaging::{
-                BringWindowToTop, DispatchMessageW, GetForegroundWindow, GetMessageExtraInfo,
-                GetWindowThreadProcessId, PeekMessageW, SetWindowsHookExA, ShowWindow,
-                TranslateMessage, UnhookWindowsHookEx, HHOOK, MSG, PM_REMOVE, SHOW_WINDOW_CMD,
-                WH_KEYBOARD_LL, WINDOWS_HOOK_ID,
-            },
+use windows::Win32::{
+    Foundation::{BOOL, HWND, LPARAM, POINT, WPARAM},
+    System::Threading::{AttachThreadInput, GetCurrentThreadId},
+    UI::{
+        Input::KeyboardAndMouse::{SendInput, INPUT},
+        WindowsAndMessaging::{
+            BringWindowToTop, DispatchMessageW, GetForegroundWindow, GetMessageExtraInfo,
+            GetMessageW, GetWindowThreadProcessId, PeekMessageW, ShowWindow, TranslateMessage, MSG,
+            PM_REMOVE, SHOW_WINDOW_CMD,
         },
     },
 };
@@ -41,24 +31,23 @@ pub struct WinHandles {
 pub struct OSApp {
     pub initialized: bool,
     pub handles: WinHandles,
-    pub hook_id: HHOOK,
+    pub msg: MSG,
 }
 impl App for OSApp {
     fn new() -> Self {
-        let hook_id = unsafe {
-            let hmodule: &mut HMODULE = &mut HMODULE(0);
-            let _ = GetModuleHandleExA(
-                GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
-                PCSTR(null()),
-                hmodule,
-            );
-            SetWindowsHookExA(WH_KEYBOARD_LL, Some(wndproc), *hmodule, 0).unwrap()
-        };
-
         Self {
             initialized: false,
             handles: WinHandles::default(),
-            hook_id,
+            msg: MSG {
+                hwnd: HWND(0),
+                message: 0,
+                wParam: WPARAM(0),
+                lParam: LPARAM(0),
+                time: 0,
+                pt: POINT {
+                    ..Default::default()
+                },
+            },
         }
     }
 
@@ -67,18 +56,22 @@ impl App for OSApp {
     }
 }
 
-impl Drop for OSApp {
-    fn drop(&mut self) {
-        unsafe {
-            let _ = UnhookWindowsHookEx(self.hook_id);
-        };
-    }
-}
-
 pub fn update(state: &mut MutexGuard<OSApp>) {
     assert!(state.handles.app != state.handles.target);
 
+    message_loop(state);
     update_hwnd(state);
+}
+
+fn message_loop(state: &mut MutexGuard<OSApp>) {
+    unsafe {
+        let pm = GetMessageW(&mut state.msg, state.handles.app, 0, 0);
+        if pm.as_bool() {
+            println!("Message Loop");
+            TranslateMessage(&state.msg);
+            DispatchMessageW(&state.msg);
+        }
+    }
 }
 
 fn update_hwnd(state: &mut MutexGuard<OSApp>) {
@@ -111,41 +104,14 @@ pub fn run_macro(state: &mut MutexGuard<OSApp>) {
     unsafe {
         force_foreground_window(state.handles.target);
 
-        println!("SendMessageW: {:?}", GetLastError());
         const CBSIZE: i32 = size_of::<INPUT>() as i32;
-        let extra_info = GetMessageExtraInfo().0.unsigned_abs();
-        let mut pinputs: &[INPUT] = &[
-            INPUT {
-                r#type: INPUT_KEYBOARD,
-                Anonymous: INPUT_0 {
-                    ki: KEYBDINPUT {
-                        wVk: VK_LWIN,
-                        dwFlags: KEYBD_EVENT_FLAGS(0),
-                        wScan: 1,
-                        time: 0,
-                        dwExtraInfo: extra_info,
-                    },
-                },
-            },
-            INPUT {
-                r#type: INPUT_KEYBOARD,
-                Anonymous: INPUT_0 {
-                    ki: KEYBDINPUT {
-                        wVk: VK_LWIN,
-                        dwFlags: KEYEVENTF_KEYUP,
-                        wScan: 1,
-                        time: 0,
-                        dwExtraInfo: extra_info,
-                    },
-                },
-            },
-        ];
+        let mut pinputs: &[INPUT] = &get_inputs(state);
         SendInput(&mut pinputs, CBSIZE);
-        println!("SendInput: {:?}", GetLastError());
     }
 }
 
-extern "system" fn wndproc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    println!("wndproc: ({:? }, {:?}, {:?})", code, wparam, lparam);
-    LRESULT(0)
+pub fn get_inputs(state: &mut MutexGuard<OSApp>) -> Box<[INPUT]> {
+    let _extra_info = unsafe { GetMessageExtraInfo().0.unsigned_abs() };
+
+    Box::new([])
 }
