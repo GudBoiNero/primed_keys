d72e8e148a4e4bb45402ff9f57f0b5531361ce62
Benjamin Champagne <80719767+GudBoiNero@users.noreply.github.com>
2024-05-14 11:27:05 -0400

    Revert "Revert "Minimal Input Running""

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_doc_comments)]
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
@@ -8,6 +9,7 @@ pub(crate) mod macos;
 #[cfg(windows)]
 pub(crate) mod windows;
 
+use app::ThreadPriority;
 #[cfg(target_os = "linux")]
 use linux as os;
 #[cfg(target_os = "macos")]
@@ -19,10 +21,8 @@ mod app;
 
 use crate::app::App;
 use os::app::OSApp;
-use std::{
-    sync::{Arc, Mutex},
-    thread,
-};
+use priomutex::Mutex;
+use std::{sync::Arc, thread};
 use tauri::{Runtime, State};
 
 pub struct AppState(pub Arc<Mutex<OSApp>>);
@@ -35,29 +35,44 @@ impl AppState {
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn run_macro(_name: String, _state: State<AppState>) -> Result<(), ()> {
+    // TODO: Turn `_state` to `state` into macro.
+    let lock = _state.0.lock(ThreadPriority::Command.into());
+    let lock = match lock {
+        Ok(x) => x,
+        Err(_) => return Err(()),
+    };
+    let mut state = lock;
+
+    os::app::run_macro(&mut state);
+
     Ok(())
 }
 
 #[tauri::command]
 // https://github.com/tauri-apps/tauri/discussions/4775
 fn init<R: Runtime>(_window: tauri::Window<R>, state: State<'_, AppState>) -> Result<(), String> {
-    let mut init_lock = state.0.lock().unwrap();
+    let mut init_lock = state.0.lock(ThreadPriority::Main.into()).unwrap();
     if init_lock.initialized {
+        println!("App Already Initialized! (Did the page refresh?)");
         return Err("App already initialized.".to_owned());
     } else {
         init_lock.initialized = true;
+        if cfg!(windows) {
+            init_lock.handles.app = ::windows::Win32::Foundation::HWND(_window.hwnd().unwrap().0);
+        }
+        println!("App Initialized.");
     }
 
     let arc = Arc::clone(&state.0);
     thread::spawn(move || loop {
-        let lock = arc.try_lock();
+        let lock = arc.lock(ThreadPriority::Main.into());
         // Handle errors, unwrap if you want
         if lock.is_err() {
-            break;
+            continue;
         }
         let lock = match lock {
             Ok(x) => x,
-            Err(_) => break,
+            Err(_) => continue,
         };
 
         // Here's your state
